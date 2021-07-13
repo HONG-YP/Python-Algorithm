9e221f41b7f463e254b91cbab14f74e95dfa1614
HONG_YP <49358347+HONG-YP@users.noreply.github.com>
2021-07-13 12:14:44 +0900

    Create 모험가 길드.py

---
+++ PreviousTest/모험가 길드.py
@@ -0,0 +1,18 @@
+N = int(input())
+guild = list(map(int, input().split()))
+
+guild.sort()
+
+group = 0
+result = 0
+
+for i in guild:
+  group += 1
+  if i >= group:
+    result += 1
+    
+print(result)
+
+# KeyPoint
+# 1. 오름차순 정렬
+# 2. 데이터가 그룹의 수보다 크거나 같은지 확인
